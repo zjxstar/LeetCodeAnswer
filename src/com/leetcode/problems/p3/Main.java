5a05fe81deaca63f4725367d841ba22a8bf7877d
zhoujingxin <zhoujingxin@360.cn>
2018-10-18 14:17:27 +0800

    增加p3解答

---
+++ src/com/leetcode/problems/p3/Main.java
@@ -0,0 +1,32 @@
+package com.leetcode.problems.p3;
+
+/**
+ * Given a string, find the length of the longest substring without repeating characters.
+ * Example 1:
+ *  Input: "abcabcbb"
+ *  Output: 3
+ *  Explanation: The answer is "abc", with the length of 3.
+ *
+ * Example 2:
+ *  Input: "bbbbb"
+ *  Output: 1
+ *  Explanation: The answer is "b", with the length of 1.
+ *
+ * Example 3:
+ *  Input: "pwwkew"
+ *  Output: 3
+ *  Explanation: The answer is "wke", with the length of 3.
+ *              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(solution.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(solution.lengthOfLongestSubstring(""));
+        System.out.println(solution.lengthOfLongestSubstring(null));
+    }
+
+}
---
+++ src/com/leetcode/problems/p3/Solution.java
@@ -0,0 +1,39 @@
+package com.leetcode.problems.p3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    /**
+     * 思路：滑动窗口
+     * 定义一个Map用来存放子串，其中key为子串的字符，value为子串的下标+1
+     * 定义两个指针i,j；j向前移动，查看s[j]是否在Map中；
+     * 如果在Map中，则出现字符重复，此时需要移动指针i，而i应该直接移动到重复字符的后一位
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        int len = s.length();
+        int maxLen = 0; // 记录最长子串长度
+        // 记录子串，key为char，value为下标，从1开始方便移动
+        Map<Character, Integer> subStrMap = new HashMap<>();
+        char c;
+        for (int i = 0, j = 0; j < len; j++) {
+            c = s.charAt(j);
+            if (subStrMap.containsKey(c)) {
+                // 找到了重复字符，则移动i
+                i = Math.max(subStrMap.get(c), i);
+            }
+            maxLen = Math.max(maxLen, j - i + 1);
+            subStrMap.put(c, j + 1); // 这里是记录的是下标+1，方便i移动
+        }
+
+        return maxLen;
+    }
+
+}
