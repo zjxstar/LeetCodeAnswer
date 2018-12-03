86e6ad8120066194de69d90dbb26c732ee8994b2
zjxstar <zjx1459871421@163.com>
2018-12-03 23:12:26 +0800

    p1盛更多的水

---
+++ src/com/leetcode/problems/p11/ContainerWithMostWater.java
@@ -0,0 +1,9 @@
+package com.leetcode.problems.p11;
+
+public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/com/leetcode/problems/p11/Solution.java
@@ -0,0 +1,18 @@
+package com.leetcode.problems.p11;
+
+public class Solution {
+
+    public int maxArea(int[] height) {
+        if (height == null || height.length < 2) {
+            return 0;
+        }
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                max = Math.max(max, Math.min(height[i], height[j]) * (j - i));
+            }
+        }
+        return max;
+    }
+
+}
