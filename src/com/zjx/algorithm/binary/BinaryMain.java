d9f2825ac0eee3a51fdcd64d09bf43f69b25c6f9
zjxstar <zjx1459871421@163.com>
2019-05-20 23:01:18 +0800

    增加整数的奇偶判断

--- src/com/zjx/algorithm/binary/BinaryMain.java
+++ src/com/zjx/algorithm/binary/BinaryMain.java
@@ -6,6 +6,14 @@ public class BinaryMain {
         BinaryProblem bp = new BinaryProblem();
         System.out.println(bp.countIntOne(-8));
         System.out.println(bp.countIntOne2(0));
+        System.out.println(bp.isOdd(0));
+        System.out.println(bp.isOdd(2));
+        System.out.println(bp.isOdd(3));
+        System.out.println(bp.isOdd(1));
+        System.out.println(bp.isEven(0));
+        System.out.println(bp.isEven(2));
+        System.out.println(bp.isEven(3));
+        System.out.println(bp.isEven(1));
     }
 
 }
--- src/com/zjx/algorithm/binary/BinaryProblem.java
+++ src/com/zjx/algorithm/binary/BinaryProblem.java
@@ -37,4 +37,24 @@ public class BinaryProblem {
         return count;
     }
 
+    /**
+     * 判断是否为奇数
+     * 奇数的二进制最后一位一定是1
+     * @param n
+     * @return
+     */
+    public boolean isOdd(int n) {
+        return (n & 1)  == 1;
+    }
+
+    /**
+     * 判断是否为偶数
+     * 先右移1位再左移一位，奇数的话就不会还原
+     * @param n
+     * @return
+     */
+    public boolean isEven(int n) {
+        return n >> 1 << 1 == n;
+    }
+
 }
